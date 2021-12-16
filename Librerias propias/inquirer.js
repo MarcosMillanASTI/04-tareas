63eb81893ca188666b2935fac1292fc6570b6782
Marcos <marcos.millan@asti.tech>
2021-12-16 16:35:07 +0100

    fin parte 50. Opciones del menú interactivo

--- Librerias propias/inquirer.js
+++ Librerias propias/inquirer.js
@@ -5,9 +5,41 @@ require('colors')
 const preguntas =[
     {
         type: 'list',
-        name: 'opciones',
+        name: 'resultado',
         message: '¿Que quieres hacer?',
-        choices: ['opt1', 'opt2', 'opt3']
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },   
+            {
+                value: '2',
+                name: '2. Listar tarea'
+            },
+            {
+                value: '3',
+                name: '3. Listar tarea completadas'
+            },
+            {
+                value: '4',
+                name: '4. Listar tarea pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar tarea'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            },
+              
+              
+              
+        ]
     }
 ];
 
@@ -17,10 +49,25 @@ const inquirerMenu = async () => {
     console.log('Seleccione un opción'.green);
     console.log('============================\n'.green);
 
-    const opt = await inquirer.prompt(preguntas);
-    return opt;
+    const {resultado} = await inquirer.prompt(preguntas);
+    return resultado;
+}
+
+const pausa = async() =>{
+    
+    const question = [
+        {
+        type: 'input',
+        name: 'enter',
+        message: `\nPresione ${'enter'.green} para continuar\n`
+        }
+    ];
+
+    await inquirer.prompt(question);
+    
 }
 
 module.exports={
-    inquirerMenu
+    inquirerMenu, 
+    pausa
 }
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 require('colors');
 //const {mostrarMenu, pausa} = require('./Librerias propias/mensajes')
-const {inquirerMenu} = require('./Librerias propias/inquirer')
+const {inquirerMenu, pausa} = require('./Librerias propias/inquirer')
 console.clear();
 
 const main = async()=>{
@@ -13,7 +13,7 @@ const main = async()=>{
 
         opt = await inquirerMenu();
         console.log(opt);
-
+        if(opt !== 0) await pausa();
     }while(opt!== '0');
     
     
