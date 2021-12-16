3c062047035210b51240cb7412e73fa46778fb1d
Marcos <marcos.millan@asti.tech>
2021-12-16 16:55:06 +0100

    fin parte 51. Lógica para el manejo de las tareas por hacer

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 require('colors');
 //const {mostrarMenu, pausa} = require('./Librerias propias/mensajes')
 const {inquirerMenu, pausa} = require('./Librerias propias/inquirer')
+const { Tarea } = require('./models/tarea')
+const { Tareas } = require('./models/tareas')
+
+
 console.clear();
 
 const main = async()=>{
@@ -11,10 +15,22 @@ const main = async()=>{
         //console.log(opt)
         //if(opt !== '0') await pausa();
 
+        /**
         opt = await inquirerMenu();
         console.log(opt);
         if(opt !== 0) await pausa();
-    }while(opt!== '0');
+         */
+        const tarea = new Tarea('Comparar comida');
+        console.log(tarea);
+        const tareas = new Tareas('Comparar comida');
+        console.log(tareas);
+
+        tareas._listado[tarea.id] = tarea;
+        console.log(tareas)
+        
+        await pausa();
+
+    }while(opt !== '0');
     
     
 }
